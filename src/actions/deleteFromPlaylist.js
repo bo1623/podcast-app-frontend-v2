b87a2ecc3bda6fddc591856fece91ef635fc1f55
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-10-10 12:09:59 +0800

    More work on deleteFromPlaylist

--- src/actions/deleteFromPlaylist.js
+++ src/actions/deleteFromPlaylist.js
@@ -1,16 +1,18 @@
 import baseUrl from '../fetchUrl'
 
 const deleteFromPlaylist = (episode_id) =>{
-  return dispatch{
-    fetch(baseUrl+'/savedepisodes',{
-      method:'DELETE',
+  return dispatch => {
+    fetch(baseUrl+'/savedepisodes/delete',{
+      method:'POST',
       headers:{
         "Content-Type":"application/json",
         "Accept": "application/json"
       },
-      body: JSON.stringify(episode_id)
+      body: JSON.stringify({episode_id: episode_id})
     })
     .then(resp=>resp.json())
     .then(json=>dispatch({type:"REMOVE_FROM_PLAYLIST",playlist:json}))
   }
 }
+
+export default deleteFromPlaylist
