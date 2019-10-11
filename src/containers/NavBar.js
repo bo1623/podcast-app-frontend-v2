b2c2660e0afde00e47b8bd94eeac29e714d357ed
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-10-11 17:53:26 +0800

    Done with styling for now

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -6,8 +6,9 @@ import Login from '../components/Login'
 const NavBar = () => {
   return (
     <div className='navbar'>
-      <NavLink style={{padding: '12px'}} to='/'>Home</NavLink> 
-      <NavLink style={{padding: '12px'}} to='/podcasts'>Podcasts</NavLink>
+      <span className="logo">THE PODCAST APP</span>
+      <NavLink  to='/'>Home</NavLink>
+      <NavLink  to='/podcasts'>Podcasts</NavLink>
       <Login />
     </div>
   );
