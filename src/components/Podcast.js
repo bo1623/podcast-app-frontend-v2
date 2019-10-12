8ab070f4bb362a626767fbff1ee0fc59dbaa753a
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-10-12 16:34:31 +0800

    Replaced previous tooltip with a different one, problem with rendering new podcast images resolved

--- src/components/Podcast.js
+++ src/components/Podcast.js
@@ -6,19 +6,39 @@ import {
 } from 'react-router-dom';
 import {connect} from 'react-redux'
 import fetchPodcast from '../actions/fetchPodcast'
-import Tooltip from 'react-png-tooltip'
+import Tooltip from 'react-png-tooltip';
+import Popover from 'react-tiny-popover';
 import {Markup} from 'interweave';
 
 
 class Podcast extends Component{
 
+  state={
+    isPopoverOpen: false
+  }
+
   render(){
+    const {isPopoverOpen} = this.state
     const {podcast} = this.props
     return(
         <div className='podcast'>
-          <Tooltip className="tooltip-box" tooltip={<img src={podcast.image}/>}>
-            Description: <Markup content={podcast.description}/>
-          </Tooltip>
+        <Popover
+          isOpen={isPopoverOpen}
+          containerClassName="popup-container"
+          position={'right'} // preferred position
+          content={(
+              <div>
+                  <p id="podcast-description-popup">Description:</p>
+                  <Markup content={podcast.description}/>
+              </div>
+          )}
+        >
+        <img
+          src={podcast.image}
+          onMouseLeave={() => this.setState({ isPopoverOpen: false })}
+          onMouseOver={() => this.setState({ isPopoverOpen: true })} />
+        </Popover>
+
           <div className='podcast-details' >
             <Link
               key={podcast.podcast_id}
@@ -39,3 +59,8 @@ const mapDispatchToProps = dispatch => ({
 })
 
 export default connect(null,mapDispatchToProps)(Podcast);
+
+//
+// <Tooltip className="tooltip-box" tooltip={<img src={podcast.image}/>}>
+//   Description: <Markup content={podcast.description}/>
+// </Tooltip>
